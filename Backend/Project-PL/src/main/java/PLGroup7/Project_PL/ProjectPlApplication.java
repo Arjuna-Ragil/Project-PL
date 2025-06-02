6bf596e16b93ba401cecb9f9157433a3e0b5af1a
Arjuna <arjunaragilputera@gmail.com>
2025-06-02 19:52:12 +0700

    Initial Commit

---
+++ Backend/Project-PL/src/main/java/PLGroup7/Project_PL/ProjectPlApplication.java
@@ -0,0 +1,13 @@
+package PLGroup7.Project_PL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPlApplication.class, args);
+	}
+
+}
---
+++ Backend/Project-PL/src/test/java/PLGroup7/Project_PL/ProjectPlApplicationTests.java
@@ -0,0 +1,13 @@
+package PLGroup7.Project_PL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
